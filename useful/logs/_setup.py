8ccdb533fba424e4bffeaa9cc27cbcbecc1aaa2c
Tomislav Levanic <tomo.levanic@gmail.com>
2020-01-14 16:20:56 +0100

    Make PEP8 compliant

--- useful/logs/_setup.py
+++ useful/logs/_setup.py
@@ -46,7 +46,7 @@ class AddSrcJsonFormatter(jsonlogger.JsonFormatter):
     JSON log.
     """
     def add_fields(self, log_record, record, message_dict):
-        super(AddSrcJsonFormatter, self).add_fields(log_record, record, message_dict)
+        super().add_fields(log_record, record, message_dict)
         # add {"src": "python"} to log record
         log_record["src"] = "python"
 
